1862a80bbc97fdbc57460433eec6e3763090435d
Andrej Hohnjec <andrej.hohnjec@planetsg.com>
2016-09-07 10:56:35 +0200

    updated migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,8 +18,6 @@ class DatabaseSeeder extends Seeder {
 		Model::unguard();
 
         $this->call('RoleTableSeeder');
-        $this->call('UserTableSeeder');
-        $this->call('RoleUserTableSeeder');
 	}
 
 }
@@ -46,80 +44,6 @@ class RoleTableSeeder extends Seeder {
         foreach($roles as $role)
         {
             Role::create($role);
-        }
-    }
-}
-
-class UserTableSeeder extends Seeder {
-
-    public function run()
-    {
-        DB::table('users')->delete();
-
-        $users = [
-            [
-                'name' => 'Admin',
-                'email' => 'admin@admin.com',
-                'password' => bcrypt('adminadmin'),
-                'img' => 'storage/images/default.png',
-            ],
-            [
-                'name' => 'UserA',
-                'email' => 'user1@user.com',
-                'password' => bcrypt('adminadmin'),
-                'img' => 'storage/images/default.png',
-            ],
-            [
-                'name' => 'UserB',
-                'email' => 'user2@user.com',
-                'password' => bcrypt('adminadmin'),
-                'img' => 'storage/images/default.png',
-            ],
-            [
-                'name' => 'UserC',
-                'email' => 'user3@user.com',
-                'password' => bcrypt('adminadmin'),
-                'img' => 'storage/images/default.png',
-            ]
-
-        ];
-        DB::table('users')->delete();
-        foreach($users as $user)
-        {
-            User::create($user);
-        }
-    }
-}
-
-class RoleUserTableSeeder extends Seeder {
-
-    public function run()
-    {
-        DB::table('role_user')->delete();
-
-        $roles = [
-            [
-                'user_id' => 1,
-                'role_id' => 1,
-            ],
-            [
-                'user_id' => 2,
-                'role_id' => 2,
-            ],
-            [
-                'user_id' => 3,
-                'role_id' => 2,
-            ],
-            [
-                'user_id' => 4,
-                'role_id' => 2,
-            ]
-
-        ];
-        DB::table('role_user')->delete();
-        foreach($roles as $role)
-        {
-            DB::table('role_user')->insert($role);
         }
     }
 }
